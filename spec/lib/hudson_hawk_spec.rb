62aeb1a6ba84a68cd3c7b9cbccdc0cf17bc0e058
Joel Helbling <joel.helbling@leandog.com>
2011-01-21 13:01:17 -0500

    moved class from HudsonHawk to HudsonHawk::Server

--- spec/lib/hudson_hawk_spec.rb
+++ spec/lib/hudson_hawk_spec.rb
@@ -1,53 +1,55 @@
-class HudsonHawk
-  attr_reader :server, :project
+module HudsonHawk
+  class Server
+    attr_reader :url, :project
 
-  def initialize(server, project)
-    @server = server
-    @project = project
-  end
+    def initialize(url, project)
+      @url = url
+      @project = project
+    end
 
-  def jobs_url
-    json_url("#{@server}/#{@project}/jobs")
-  end
+    def jobs_url
+      json_url("#{@url}/#{@project}/jobs")
+    end
 
-  def job_url(job_number)
-    json_url("#{@server}/#{@project}/#{job_number}")
-  end
+    def job_url(job_number)
+      json_url("#{@url}/#{@project}/#{job_number}")
+    end
 
-  def json_url(url)
-    "#{ url.gsub(/\/+$/, '') }/api/json"
+    def json_url(url)
+      "#{ url.gsub(/\/+$/, '') }/api/json"
+    end
   end
 end
 
 describe HudsonHawk do
   before do
-    @server = 'http://my.hudson.server:9080'
+    @url = 'http://my.hudson.server:9080'
     @project = 'myProject'
-    @hh = HudsonHawk.new(@server, @project)
+    @server = HudsonHawk::Server.new(@url, @project)
   end
 
-  it "should have a server" do
-    @hh.server.should == @server
+  it "should have a url" do
+    @server.url.should == @url
   end
 
   it "should have a project" do
-    @hh.project.should == @project
+    @server.project.should == @project
   end
 
   it "should convert a Hudson URL to a Hudson API URL" do
     url = 'http://some.hudson.server/hudson/myProject/jobs'
-    @hh.json_url(url).should == "#{url}/api/json"
+    @server.json_url(url).should == "#{url}/api/json"
     url2 = 'foo/'
-    @hh.json_url(url2).should == "foo/api/json"
+    @server.json_url(url2).should == "foo/api/json"
     url3 = 'foo//'
-    @hh.json_url(url3).should == "foo/api/json"
+    @server.json_url(url3).should == "foo/api/json"
   end
 
   it "should return a jobs URL" do
-    @hh.jobs_url.should == "#{@server}/#{@project}/jobs/api/json"
+    @server.jobs_url.should == "#{@url}/#{@project}/jobs/api/json"
   end
 
   it "should return a job URL" do
-    @hh.job_url("1001").should == "#{@server}/#{@project}/1001/api/json"
+    @server.job_url("1001").should == "#{@url}/#{@project}/1001/api/json"
   end
 end
